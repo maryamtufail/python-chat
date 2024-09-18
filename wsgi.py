980bbdeb426d433da7eb6a18f38af3d669fcc991
Maryam Tufail <maryamtufail78@gmail.com>
2024-09-18 21:20:16 +0500

    deply-issue-resolve

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,11 @@
+from gevent.pywsgi import WSGIServer
 from app import app, socketio
 
+# Run the server with Gevent
 if __name__ == "__main__":
-    socketio.run(app)
+    # Wrap the app with SocketIO to support WebSockets
+    socketio.init_app(app)
+    
+    # Create and start the WSGI server
+    http_server = WSGIServer(('0.0.0.0', 8080), app)
+    http_server.serve_forever()
